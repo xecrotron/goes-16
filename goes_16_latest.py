157e2eff48017b5879e944e0501a53c0be992c65
Umang Kalra <umangkalra10@gmail.com>
2023-09-30 18:48:38 +0000

    Stable- output filenamng changed

--- goes_16_latest.py
+++ goes_16_latest.py
@@ -10,7 +10,12 @@ from osgeo import osr
 from PIL import Image
 import logging
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.INFO,
+    format='%(asctime)s %(levelname)-8s %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S',
+    filename="goes_downloader.log", 
+    filemode="a"
+)
 
 class GoesDownloaderLatest:
     def __init__(self) -> None:
