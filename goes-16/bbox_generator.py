506fa6851237cf46939053114112c309dea0b28e
Solarix <xecro@pop-os.localdomain>
2024-02-07 16:42:32 +0530

    bbox_generator

--- goes-16/bbox_generator.py
+++ goes-16/bbox_generator.py
@@ -1,98 +1,95 @@
 import os
-import argparse
 import shapely
 import fiona
 import json
 import numpy as np
 from pyproj import Proj,transform
 from shapely.geometry import shape
-from geojson import Polygon, Feature, FeatureCollection
-
-# from geojson import Polygon, Feature, FeatureCollection
-# from geopy.distance import geodesic
+from geojson import Polygon, Feature
 
 def generate_bounding_box_geojson(center_point, startDate, endDate, box_size_acres=10):
-
     box_size_km = box_size_acres*0.00404686
     center_latitude, center_longitude = center_point
 
-    # Calculate bounding box corners
-    min_latitude = center_latitude - (box_size_km / 2) / 111.32  # 1 degree of latitude is approximately 111.32 km
-    max_latitude = center_latitude + (box_size_km / 2) / 111.32
-    min_longitude = center_longitude - (box_size_km / 2) / (111.32 * abs(np.cos(np.radians(center_latitude))))
-    max_longitude = center_longitude + (box_size_km / 2) / (111.32 * abs(np.cos(np.radians(center_latitude))))
+    min_latitude = center_latitude - (box_size_km / 2) / 111.111
+    max_latitude = center_latitude + (box_size_km / 2) / 111.111
+    min_longitude = center_longitude - (box_size_km / 2) / (111.321 * abs(np.cos(np.radians(center_latitude))))
+    max_longitude = center_longitude + (box_size_km / 2) / (111.321 * abs(np.cos(np.radians(center_latitude))))
 
     # Create GeoJSON Polygon
     polygon_coordinates = [
-        (min_longitude, min_latitude),
         (min_longitude, max_latitude),
-        (max_longitude, max_latitude),
+        (min_longitude, min_latitude),
         (max_longitude, min_latitude),
-        (min_longitude, min_latitude)
+        (max_longitude, max_latitude),
+        (min_longitude, max_latitude)
     ]
-    polygon = Polygon([polygon_coordinates])
-
-    # Create GeoJSON Feature
+    polygon=Polygon()
+    polygon['coordinates']=[polygon_coordinates]
     feature = Feature(geometry=polygon)
-    # features['properties'
     feature.properties['Start Date']=startDate
     feature.properties['End Date']=endDate
 
-    return feature
+    finalJson ={
+        "type":"FeatureCollection",
+        "features":[feature]
+    }
+    return finalJson
 
 
-def shapelyFile(root,files):
+def dumpjson(root:os, bounding_box_geojson, filename:str):
+    json_dump_dir = os.path.join(os.path.dirname(root),'geojson')
+    filename+=".geojson"
+    with open(os.path.join(json_dump_dir,filename),'w') as json_file:
+        json.dump(bounding_box_geojson, json_file,indent=2)
+
+
+#reading 
+def shapelyFile(root:os,files:os):
     try:
         for file in files:
             if file.endswith(".shp") == False:
                 continue
             with fiona.open(os.path.join(root, file)) as shp:
-                crs = shp.crs
-                epsg_code = crs['init'].split(':')[1]
-                print("EPSG Code:", epsg_code)
+
+                # To see EPSG Code
+                # crs = shp.crs
+                # epsg_code = crs['init'].split(':')[1]
+                # print("EPSG Code:", epsg_code)
+
                 for feature in shp:
-                    # Extract the geometry
                     
                     geometry = feature['geometry']
                     property = feature['properties']
                     shapely_geometry = shape(geometry)
-                    # Convert the geometry to a Shapely object
 
+                    sourceOID = property["poly_Sourc"]
                     acres= property['poly_GISAc']
-                    # latitude = property['']
                     fireDisoveryDateTime = property['attr_Fir_7']
                     fireControlDateTime = property['attr_Contr']
                     bounding_box = shapely_geometry.bounds
                     if acres<10.0:
                         continue
+
+                    # To get all attributes in property
                     # for prop,val in vars(property)['_data'].items():
                     #     print(prop," : ", val)
 
-                    print("start date: ", fireDisoveryDateTime )
-                    print("End Date: ",fireControlDateTime)
-                    print("Acres: ", acres)
-                    print("Geometry Type:", shapely_geometry.geom_type)
-                    print("Area:", shapely_geometry.area)
-                    print("Bounding Box:", shapely_geometry.bounds)
-                    bounding_box_geojson = generate_bounding_box_geojson([ (bounding_box[0]+bounding_box[2])/2, (bounding_box[1]+bounding_box[3])/2 ], fireDisoveryDateTime, fireControlDateTime)
-                    print(bounding_box_geojson)
-
+                    bounding_box_geojson = generate_bounding_box_geojson(( (bounding_box[1]+bounding_box[3])/2, (bounding_box[0]+bounding_box[2])/2 ), fireDisoveryDateTime, fireControlDateTime)
+                    dumpjson(root, bounding_box_geojson,str(sourceOID))
     except Exception as e:
         print("Exception Occurred")
         print(e)
 
-def main(new_path: os):
-    # print(new_path)
-    print(os.path.isdir(new_path))
+# Give path for script
+def read_file(new_path: os):
     for root, dir, files in os.walk(new_path):
         shapelyFile(root,files)
         
 
-def read_files() -> None:
+def main() -> None:
     base_path = os.getcwd()
-    new_path = os.path.join(base_path,"nifc")
-    # print(new_path)
-    main(new_path)
-    
+    new_path = os.path.join(base_path,"DATA")
+    read_file(new_path)
 
-read_files()
+main()
